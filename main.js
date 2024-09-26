c69677fd4d3f57421832078f21f1b8c4ab5020c7
Havicish <havicish5@gmail.com>
2024-09-26 16:38:54 -0700

    Changed Choco Cows

--- main.js
+++ main.js
@@ -3,18 +3,35 @@ let Milk = 50;
 let ChocoMilk = 0;
 let Cows = 1;
 let PriceCow = 10;
-let Time = 3000;
+let Time = 6000;
 let CheeseMakers = 0;
 let PriceCheeseMaker = 20;
 let ChocoCows = 0;
 let PriceChocoCow = 100;
+
+let ChocoCowUnlocked = false;
+
+document.addEventListener("DOMContentLoaded", () => {
+    ShowUnlocks()
+});
+
+function ShowUnlocks() {
+    const elements = document.querySelectorAll("#ChocoCowDiv");
+    elements.forEach(element => {
+        if (!ChocoCowUnlocked) {
+            element.style.display = "none";
+        } else {
+            element.style.display = "default";
+        }
+    });
+}
 
 function Get(Id) {
     return document.getElementById(Id);
 }
 
 function MakeCheeseFunc() {
-    if (Milk > 0) {
+    if (Math.round(Milk) > 0) {
         Cheese++;
         Milk--;
         Get("CheeseCount").innerText = Math.round(Cheese);
@@ -28,7 +45,7 @@ function MilkCowFunc() {
 }
 
 function BuyCowFunc() {
-    if (Cheese >= PriceCow) {
+    if (Math.round(Cheese) >= PriceCow) {
         Cows++;
         Cheese -= PriceCow;
         PriceCow += 10;
@@ -43,7 +60,7 @@ function MilkChocoCowFunc() {
 }
 
 function BuyChocoCowFunc() {
-    if (Cheese >= PriceChocoCow) {
+    if (Math.round(Cheese) >= PriceChocoCow) {
         ChocoCows++;
         Cheese -= PriceChocoCow;
         PriceChocoCow += 20;
@@ -53,7 +70,7 @@ function BuyChocoCowFunc() {
 }
 
 function BuyCheeseMakerFunc() {
-	if (Cheese >= PriceCheeseMaker) {
+	if (Math.round(Cheese) >= PriceCheeseMaker) {
 		CheeseMakers++;
 		Cheese -= PriceCheeseMaker;
 		PriceCheeseMaker = Math.round(PriceCheeseMaker * 1.15 / 5) * 5;
@@ -66,11 +83,17 @@ function BuyCheeseMakerFunc() {
 setInterval(() => {
     Get("CheeseCount").innerText = Math.round(Cheese);
     Get("MilkCount").innerText = Math.round(Milk);
+    if (Math.round(Cheese) == 3621)
+        Get("CheeseCount").innerText = "E621";
+
+    if (Cheese >= 100)
+        ChocoCowUnlocked = true
+    ShowUnlocks()
 
 	if (Milk >= CheeseMakers / 10 / 2) {
 		Cheese += CheeseMakers / 10 / 2;
 		Milk -= CheeseMakers / 10 / 2;
-		Get("CheesePerSecond").innerText = CheeseMakers / 2;
+		Get("CheesePerSecond").innerText = Math.round(CheeseMakers / 2 * 10) / 10;
 	} else {
 		Get("CheesePerSecond").innerText = Milk;
 		Cheese += Milk;
