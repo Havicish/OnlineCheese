f135727ee7825db3989b109a351ac6d764a7850f
Havicish <157093976+Havicish@users.noreply.github.com>
2024-10-03 17:12:27 -0700

    Update main.js

--- main.js
+++ main.js
@@ -6,16 +6,19 @@ let Cows = 1;
 let PriceCow = 10;
 let Time = 6000;
 let CheeseMakers = 0;
-let PriceCheeseMaker = 20;
+let ChocoCheeseMakers = 0;
+let PriceCheeseMaker = 10;
+let PriceChocoCheeseMaker = 30;
 let ChocoCows = 0;
 let PriceChocoCow = 100;
 let SpecCows = 0;
 
-let ChocoCowUnlocked = false;
-let SpecCowUnlocked = true;
+let ChocoCowUnlocked = true;
+let SpecCowUnlocked = false;
+let CheeseMakerUnlocked = true;
 
 document.addEventListener("DOMContentLoaded", () => {
-    ShowUnlocks()
+    ShowUnlocks();
 });
 
 function ShowUnlocks() {
@@ -31,6 +34,15 @@ function ShowUnlocks() {
     elements = document.querySelectorAll("#SpecCowDiv");
     elements.forEach(element => {
         if (!SpecCowUnlocked) {
+            element.style.display = "none";
+        } else {
+            element.style.display = "block";
+        }
+    });
+
+	elements = document.querySelectorAll("#CheeseMakerDiv");
+    elements.forEach(element => {
+        if (!CheeseMakerUnlocked) {
             element.style.display = "none";
         } else {
             element.style.display = "block";
@@ -94,43 +106,86 @@ function BuyCheeseMakerFunc() {
 	if (Math.round(Cheese) >= PriceCheeseMaker) {
 		CheeseMakers++;
 		Cheese -= PriceCheeseMaker;
-		PriceCheeseMaker = Math.round(PriceCheeseMaker * 1.15 / 5) * 5;
+		PriceCheeseMaker = Math.round(PriceCheeseMaker * 1.05);
 		Get("CheeseCount").innerText = Math.round(Cheese);
 		Get("CheeseMakerCount").innerText = CheeseMakers;
 		Get("PriceCheeseMaker").innerText = PriceCheeseMaker;
 	}
 }
 
-function FeedCowFunc() {
+function BuyChocoCheeseMakerFunc() {
+	if (Math.round(ChocoCheese) >= PriceChocoCheeseMaker) {
+		ChocoCheeseMakers++;
+		ChocoCheese -= PriceChocoCheeseMaker;
+		PriceChocoCheeseMaker = Math.round(PriceChocoCheeseMaker * 1.05);
+		Get("ChocoCheeseCount").innerText = Math.round(ChocoCheese);
+		Get("ChocoCheeseMakerCount").innerText = ChocoCheeseMakers;
+		Get("PriceChocoCheeseMaker").innerText = PriceChocoCheeseMaker;
+	}
+}
 
+function FeedSpecCowFunc() {
+	if (ChocoMilk >= SpecCows * 2) { 
+		ChocoMilk -= SpecCows;
+		SpecCows = Math.max(SpecCows, 1) * 2;
+	}
+	Get("SpecCowCount").innerText = SpecCows;
+	Get("PriceSpecCow").innerText = SpecCows * 2;
+	Get("ChocoMilkCount").innerText = Math.round(ChocoMilk);
 }
 
 function KillSpecCowFunc() {
-    if (SpecCows > 0) {
-        SpecCows--;
-    }
+	SpecCows--;
+	if (SpecCows <= 0) {
+		SpecCowUnlocked = false;
+		SpecCows = 1;
+	}
+	Get("SpecCowCount").innerText = SpecCows;
+	Get("PriceSpecCow").innerText = SpecCows * 2;
+	Get("ChocoMilkCount").innerText = Math.round(ChocoMilk);
 }
 
 setInterval(() => {
     Get("CheeseCount").innerText = Math.round(Cheese);
+	Get("ChocoCheeseCount").innerText = Math.round(ChocoCheese);
     Get("MilkCount").innerText = Math.round(Milk);
-    if (Math.round(Cheese) == 3621)
-        Get("CheeseCount").innerText = "E621";
+	Get("ChocoMilkCount").innerText = Math.round(ChocoMilk);
 
-
+	if (SpecCowUnlocked == true) {
+		Cheese += SpecCows / 32;
+		if (SpecCows > 1) { 
+			SpecCows -= Math.round(Math.random() * SpecCows / 32 - .5);
+		}
+		Get("SpecCowCount").innerText = SpecCows;
+		Get("PriceSpecCow").innerText = SpecCows * 2;
+		Get("ChocoMilkCount").innerText = Math.round(ChocoMilk);
+	}
 
     if (Math.round(Cheese) >= 100)
-        ChocoCowUnlocked = true
+        ChocoCowUnlocked = true;
+	if (Math.round(Cheese) >= 20)
+		CheeseMakerUnlocked = true;
+	if (Math.round(ChocoCheese) == 69)
+		SpecCowUnlocked = true;
     ShowUnlocks()
 
 	if (Milk >= CheeseMakers / 10 / 2) {
 		Cheese += CheeseMakers / 10 / 2;
 		Milk -= CheeseMakers / 10 / 2;
-		Get("CheesePerSecond").innerText = Math.round(CheeseMakers / 2 * 10) / 10;
+		Get("CheesePerSecond").innerText = Math.round(CheeseMakers / 2 * 10) / 10 + Math.round(SpecCows / 32 * 100) / 10;
 	} else {
 		Get("CheesePerSecond").innerText = Milk;
 		Cheese += Milk;
 		Milk = 0;
+	}
+	if (ChocoMilk >= ChocoCheeseMakers / 10 / 2) {
+		ChocoCheese += ChocoCheeseMakers / 10 / 2;
+		ChocoMilk -= ChocoCheeseMakers / 10 / 2;
+		Get("ChocoCheesePerSecond").innerText = Math.round(ChocoCheeseMakers / 2 * 10) / 10;
+	} else {
+		Get("ChocoCheesePerSecond").innerText = ChocoMilk;
+		ChocoCheese += ChocoMilk;
+		ChocoMilk = 0;
 	}
 
     Time--;
